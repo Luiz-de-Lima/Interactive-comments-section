10647795f8624fc607028d812f24d04dc36f010f
Luiz-de-Lima <luiz12359@gmail.com>
2022-08-09 01:18:01 -0300

    criação de tiprogafia

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.scss'
+import { useState } from "react";
+import Logo from "./assets/favicon-32x32.png";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      
-    </div>
-  )
+  return <div className="App">este  </div>;
 }
 
-export default App
+export default App;
