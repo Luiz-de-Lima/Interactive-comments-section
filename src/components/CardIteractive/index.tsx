fee14a41bf3af87d1ebbb5f0f6beb11259b9a698
Luiz-de-Lima <luiz12359@gmail.com>
2022-08-13 19:28:51 -0300

    informações da api na tela

--- src/components/CardIteractive/index.tsx
+++ src/components/CardIteractive/index.tsx
@@ -1,39 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import iconMinus from "../../assets/icon-minus.svg";
 import iconPlus from "../../assets/icon-plus.svg";
 import iconReply from "../../assets/icon-reply.svg";
 import profileDog from "../../assets/profiledog.png";
+import data from "../../data/data.json";
 
 import "./styles.scss";
 
 export const CardInteractive = () => {
+  const [comments, setUpdateComments] = useState(data.comments);
+
   return (
-    <div className="container">
-      <div className="container-liked">
-        <img src={iconPlus} alt="" />
-        <span>12</span>
-        <img src={iconMinus} alt="" />
-      </div>
+    <>
+      {comments.map((item) => (
+        <div className="container">
+          <div className="container-liked">
+            <img src={iconPlus} alt="" />
+            <span>{item.score}</span>
+            <img src={iconMinus} alt="" />
+          </div>
 
-      <div className="container-profile">
-        <div className="container-profile__post">
-          <img src={profileDog} alt="profile" />
-          <p className="post__profile">amyrobson</p>
-          <p>1 month ago</p>
-        </div>
+          <div className="container-profile">
+            <div className="container-profile__post">
+              <img src={`${item.user.image.png}`} alt="profile" />
+              <p className="post__profile">{item.user.username}</p>
+              <p>{item.createdAt}</p>
+            </div>
 
-        <div className="container-profile__reply">
-          <img src={iconReply} alt="" />
+            <div className="container-profile__reply">
+              <img src={iconReply} alt="" />
+            </div>
+            <div className="container-profile__comment">
+              <p>{item.content}</p>
+            </div>
+          </div>
         </div>
-        <div className="container-profile__comment">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            architecto itaque quaerat molestias error animi earum quis
-            assumenda, sint et odit iste quisquam praesentium consequatur
-            temporibus magnam sed quia dolor.
-          </p>
-        </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
